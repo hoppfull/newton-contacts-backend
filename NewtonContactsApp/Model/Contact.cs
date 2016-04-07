a2bcbd428c34f64abdbefbb77ecd2f8def6168e0
Jonas Sternerson <jonas@sternerson.se>
2016-04-07 14:01:14 +0200

    Adding City field to Contact

--- NewtonContactsApp/Model/Contact.cs
+++ NewtonContactsApp/Model/Contact.cs
@@ -13,6 +13,7 @@ namespace NewtonContactsApp.Model
         public string CareOf { get; set; }
         public string Address { get; set; }
         public string PostalCode { get; set; }
+        public string City { get; set; }
         public string Country { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
