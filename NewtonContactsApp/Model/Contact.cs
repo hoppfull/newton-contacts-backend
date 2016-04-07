7c457715e7b51660161542241c461d12f88df415
Jonas Sternerson <jonas@sternerson.se>
2016-04-07 05:31:33 +0200

    Backend interface and implementation skeleton delivered.

---
+++ NewtonContactsApp/Model/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonContactsApp.Model
+{
+    public class Contact
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public string CareOf { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string AppData { get; set; }
+    }
+}
---
+++ NewtonContactsApp/Model/IContactsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonContactsApp.Model
+{
+    public interface IContactsRepository
+    {
+        IList<Contact> GetAll();
+
+        Contact Get(int index);
+
+        int Create(Contact contact);
+
+        void Update(Contact contact);
+
+        void Delete(int index);
+
+        void SaveChanges();
+    }
+}
