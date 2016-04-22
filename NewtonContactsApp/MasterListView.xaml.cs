d8745dd69f1c87b7358dee471656dcb3c44ce37e
Björn Kihlberg <hoppfull@gmail.com>
2016-04-22 10:44:56 +0200

    initialized all XAML-pages

--- NewtonContactsApp/MasterListView.xaml.cs
+++ NewtonContactsApp/MasterListView.xaml.cs
@@ -13,10 +13,15 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
 namespace NewtonContactsApp {
-    public sealed partial class MainPage : Page {
-        public MainPage() {
-            InitializeComponent();
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MasterListView : Page {
+        public MasterListView() {
+            this.InitializeComponent();
         }
     }
 }
